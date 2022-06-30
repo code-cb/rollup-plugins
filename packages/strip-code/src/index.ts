ca7c579e7dc5908444a46d948cb19b43ebd26ec3
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-06-30 18:26:13 +0700

    First commit

---
+++ packages/strip-code/src/index.ts
@@ -0,0 +1,45 @@
+/**
+ * Reference: https://github.com/se-panfilov/rollup-plugin-strip-code
+ */
+
+import { createFilter, FilterPattern } from '@rollup/pluginutils';
+import MagicString from 'magic-string';
+import { Plugin } from 'rollup';
+
+export type StripCodeOptions = {
+  exclude?: FilterPattern;
+  include?: FilterPattern;
+} & (
+  | { pattern: string | RegExp }
+  | { endComment: string; startComment: string }
+);
+
+export const stripCode = (
+  options: StripCodeOptions = {
+    endComment: '#stripCodeEnd',
+    startComment: '#stripCodeStart',
+  },
+): Plugin => {
+  const { exclude, include } = options;
+  const filter = createFilter(include, exclude);
+  const replacePattern =
+    'pattern' in options
+      ? options.pattern
+      : new RegExp(
+          `[ \t]*\/\*\s*${options.startComment}\s\*\/([\s*\S]*?)\s*${options.endComment}\s\*\/[ \t]*`,
+          'g',
+        );
+
+  return {
+    name: 'strip-code',
+    transform(code, id) {
+      if (!filter(id)) return null;
+      const ms = new MagicString(code);
+      ms.replace(replacePattern, '');
+      return {
+        code: ms.toString(),
+        map: ms.generateMap({ hires: true }),
+      };
+    },
+  };
+};
