9ec4a7514e6d890ce354a5ce6dca3a5d4fca1f72
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-10-17 20:32:10 +0700

    Add raw-format plugin

---
+++ packages/raw-format/src/index.ts
@@ -0,0 +1,29 @@
+import MagicString from 'magic-string';
+import { Plugin } from 'rollup';
+
+const IIFE_REGEX = /\(function \(\) \{([\s\S]+)\}\)\(\);?/gm;
+
+interface RawFormatOptions {
+  sourcemap?: boolean;
+}
+
+export const rawFormat = ({ sourcemap }: RawFormatOptions): Plugin => ({
+  name: 'raw-format',
+  outputOptions(outputOptions) {
+    return { ...outputOptions, format: 'iife' };
+  },
+  renderChunk(code) {
+    const ms = new MagicString(code);
+    ms.replaceAll(IIFE_REGEX, (_, iifeBody: string) =>
+      iifeBody
+        .trim()
+        .split('\n')
+        .map(line => (line.startsWith('  ') ? line.slice(2) : line))
+        .join('\n'),
+    );
+    return {
+      code: ms.toString(),
+      map: sourcemap ? ms.generateMap({ hires: true }) : null,
+    };
+  },
+});
